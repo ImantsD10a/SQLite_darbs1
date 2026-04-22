397ab6fb2c5173a0b6eef8998caa4f656c8091a8
ImantsD10a <rolands69dim@gmail.com>
2026-04-22 12:13:56 +0300

    Create uzdevums2.py

---
+++ uzdevums2.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+conn = sqlite3.connect("biblioteka.db")
+cursor = conn.cursor()
+
+# Pievienojam vēl 2 grāmatas no viena autora (Vilis Lācis)
+cursor.execute("INSERT INTO gramatas (nosaukums, autors, gads) VALUES (?, ?, ?)",
+               ("Zvejnieka dēls", "Vilis Lācis", 1933))
+
+cursor.execute("INSERT INTO gramatas (nosaukums, autors, gads) VALUES (?, ?, ?)",
+               ("Uz jauno krastu", "Vilis Lācis", 1945))
+
+conn.commit()
+
+# Konkrēta autora grāmatas
+print("\nVilis Lācis grāmatas:")
+cursor.execute("SELECT * FROM gramatas WHERE autors = ?", ("Vilis Lācis",))
+for g in cursor.fetchall():
+    print(g)
+
+# Grāmatas pēc 2000. gada
+print("\nGrāmatas pēc 2000. gada:")
+cursor.execute("SELECT * FROM gramatas WHERE gads > 2000")
+for g in cursor.fetchall():
+    print(g)
+
+# Kārtošana pēc gada
+print("\nGrāmatas sakārtotas pēc gada:")
+cursor.execute("SELECT * FROM gramatas ORDER BY gads")
+for g in cursor.fetchall():
+    print(g)
+
+conn.close()
